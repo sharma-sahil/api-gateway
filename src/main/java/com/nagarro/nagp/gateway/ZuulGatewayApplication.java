562c5eab2ba35143df99de06c2a845b160559d33
sahilsharma <sahil.sharma@nagarro.com>
2019-11-08 23:13:54 +0530

    initial commit

---
+++ src/main/java/com/nagarro/nagp/gateway/ZuulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
