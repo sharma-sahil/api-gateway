c00dbf2dac4b842e25e7c542728c8f808d19f6e1
sahilsharma <sahil.sharma@nagarro.com>
2019-11-08 23:21:28 +0530

    rename to API Gateway

--- src/main/java/com/nagarro/nagp/gateway/ApiGatewayApplication.java
+++ src/main/java/com/nagarro/nagp/gateway/ApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZuulGatewayApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZuulGatewayApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
